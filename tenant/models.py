0a5b08b5fb5a3a93d48a165882d85a5f6dac1d46
AlexPy <indrajit.alex@gmail.com>
2022-06-03 00:58:53 +0600

    index.html updated

--- tenant/models.py
+++ tenant/models.py
@@ -5,6 +5,9 @@ class Tenant(models.Model):
     name = models.CharField(max_length=255)
     subdomain = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class TenantAwareModel(models.Model):
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
@@ -12,3 +15,6 @@ class TenantAwareModel(models.Model):
 
 class Member(TenantAwareModel):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
