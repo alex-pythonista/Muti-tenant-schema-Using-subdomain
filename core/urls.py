83f0ebd574c67986c2b5a9221cbb81e67cc29c42
AlexPy <indrajit.alex@gmail.com>
2022-06-04 00:35:53 +0600

    docker init

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 from tenant import views
 
+def home(request):
+    return HttpResponse('<h1>Hello World</h1>')
 
 urlpatterns = [
+    path('', home),
     path('our_team/', views.our_team, name='team'),
     path('admin/', admin.site.urls),
 ]
